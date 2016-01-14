a3202c362283eac7522ee90ed65efcf4f1d412d5
Anton Evers <anton.evers@kega.nl>
2016-01-14 16:13:49 +0100

    +: installer update

--- install.sh
+++ install.sh
@@ -12,7 +12,9 @@ git config --global branch.project-preview.merge refs/heads/project-preview
 git config --global branch.project-preview.remote origin
 git config --global branch.project-preview.rebase true
 
+wget "https://raw.githubusercontent.com/itnova/git-exstensions/master/git-preview?token=ABa46fJq10CEazHz3E1Rx-vb9pC8ki5Dks5WoPVBwA%3D%3D" -O git-preview || exit $?
+cp git-preview /usr/local/bin/ || exit $?
+chmod 777 /usr/local/bin/git-preview || exit $?
 
-
-echo -e "You can now use\ngit support"
+echo -e "You can now use\n- git support\n"
 echo -e "Done"
