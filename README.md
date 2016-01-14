0fa3f3304916a4182155798058697a6a1ead91c6
Anton Evers <anton.evers@kega.nl>
2016-01-14 16:20:19 +0100

    +: update installer

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Installation
 ```
-bash < <(wget -q --no-check-certificate -O - "https://raw.githubusercontent.com/itnova/git-exstensions/master/install.sh?token=ABa46YJuwYA57p2b5xONsI2c1VZ0P33sks5WoPTAwA%3D%3D")
+bash < <(wget -q --no-check-certificate -O - https://raw.githubusercontent.com/itnova/git-extensions/master/install.sh)
 ```
--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ git config --global branch.project-preview.merge refs/heads/project-preview
 git config --global branch.project-preview.remote origin
 git config --global branch.project-preview.rebase true
 
-wget  "https://raw.githubusercontent.com/itnova/git-exstensions/master/git-preview?token=ABa46fJq10CEazHz3E1Rx-vb9pC8ki5Dks5WoPVBwA%3D%3D" -O git-preview -quiet || exit $?
+wget https://raw.githubusercontent.com/itnova/git-extensions/master/git-preview -O git-preview -quiet || exit $?
 rm /usr/local/bin/git-preview || exit $?
 mv git-preview /usr/local/bin/ || exit $?
 chmod 777 /usr/local/bin/git-preview || exit $?
